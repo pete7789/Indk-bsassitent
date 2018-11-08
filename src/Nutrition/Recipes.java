b96c661bdabc9dcf93a7bdcdaf65681a01ef9b76
Murat Eroglu <murat@eroglu.dk>
2018-11-08 12:06:20 +0100

    New recipe class
    Added new class and added welcome message and input

--- src/Nutrition/Recipes.java
+++ src/Nutrition/Recipes.java
@@ -1,6 +1,21 @@
 package Nutrition;
 
+import java.util.*;
+
 public class Recipes {
+    private String RecipeName;
+
+
+    private String welcomeMessage; {
+        System.out.println("Welcome to recipe creator 101");
+    }
+
+    private String inputRecipe; {
+        System.out.println("Name");
+        Scanner sc = new Scanner(System.in);
+        RecipeName = sc.nextLine();
+    }
 
     
+
 }
