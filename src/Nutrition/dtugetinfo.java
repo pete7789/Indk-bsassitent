486bb4b8ef57fb4e618dcaecf7eea0f3899011f3
Murat Eroglu <murat@eroglu.dk>
2018-11-09 13:28:18 +0100

    CSV reader
    Added a csv reader to read the data from DTU

--- src/Nutrition/dtugetinfo.java
+++ src/Nutrition/dtugetinfo.java
@@ -1,45 +1,19 @@
 package Nutrition;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Iterator;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class dtugetinfo{
 
-    public static void main(String[] args) throws IOException {
-        File excelFile = new File("contacts.xlsx");
-        FileInputStream fis = new FileInputStream(excelFile);
 
-        // we create an XSSF Workbook object for our XLSX Excel File
-        XSSFWorkbook workbook = new XSSFWorkbook(fis);
-        // we get first sheet
-        XSSFSheet sheet = workbook.getSheetAt(0);
-
-        // we iterate on rows
-        Iterator<Row> rowIt = sheet.iterator();
-
-        while(rowIt.hasNext()) {
-            Row row = rowIt.next();
-
-            // iterate on cells for the current row
-            Iterator<Cell> cellIterator = row.cellIterator();
-
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-                System.out.print(cell.toString() + ";");
-            }
-
-            System.out.println();
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("C:\\Users\\murat\\OneDrive\\Dokumenter\\Indk-bsassitent\\src\\Nutrition\\test1.csv"));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext()){
+            System.out.print(scanner.next() + "|");
         }
-
-        workbook.close();
-        fis.close();
+        scanner.close();
     }
 
 }
