ed14ac0f107d9fb59b367891f74274148abf1331
Murat Eroglu <murat@eroglu.dk>
2018-11-23 13:16:59 +0100

    Revert "CSV reader working"
    This reverts commit 483ad3a6841cc0eed92ef5ad2b1c146b88362393.

--- src/Nutrition/dtugetinfo.java
+++ src/Nutrition/dtugetinfo.java
@@ -1,40 +1,19 @@
 package Nutrition;
 
-import com.opencsv.CSVReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class dtugetinfo{
 
-    private static final String DTU_GET_INFO = "C:\\Users\\murat\\OneDrive\\Dokumenter\\Indk-bsassitent\\src\\Nutrition\\Fodevaredata_prog_v3_new.csv";
 
-    public static void main(String[] args) throws IOException {
-           try (
-                Reader reader = Files.newBufferedReader(Paths.get(DTU_GET_INFO));
-                CSVReader csvReader = new CSVReader(reader)
-                )
-        {
-            String[] nextReading;
-            while ((nextReading = csvReader.readNext()) != null) {
-                if (nextReading[99].equals("")){
-                    nextReading[99] = "0";
-                }
-                System.out.println("Food Name: " + nextReading[0]);
-                System.out.println("kJ: " + nextReading[4] + " kJ");
-                System.out.println("kcal: " + nextReading[5] + " kcal");
-                System.out.println("Protein: " + nextReading[8] + "g");
-                System.out.println("Carbohydrate: " + nextReading[10] + "g");
-                System.out.println("Sugar: " + nextReading[99] + "g");
-                System.out.println("Salt: " + nextReading[49] + "mg");
-                System.out.println("Fat: " + nextReading[15] + "g");
-                System.out.println("Saturated Fat: " + nextReading[159] + "g");
-                System.out.println("--------------------------------------");
-
-            }
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("C:\\Users\\murat\\OneDrive\\Dokumenter\\Indk-bsassitent\\src\\Nutrition\\test1.csv"));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext()){
+            System.out.print(scanner.next() + "|");
         }
+        scanner.close();
     }
 
 }
