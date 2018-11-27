baa6be78dc8266f80e0d4f79459c8cc42db547c9
Murat Eroglu <murat@eroglu.dk>
2018-11-27 12:43:23 +0100

    Semikolon sepereret csv fil

--- src/Nutrition/dtugetinfo.java
+++ src/Nutrition/dtugetinfo.java
@@ -14,7 +14,7 @@ import java.util.Iterator;
 
 public class dtugetinfo{
 
-    private static final String DTU_GET_INFO = "src/Nutrition/Fodevaredata_prog_v3_new.csv";
+    private static final String DTU_GET_INFO = "src/Nutrition/Fodevaredata_prog_v3_new2.csv";
 
         public static void main(String[] args) throws IOException {
            try (
