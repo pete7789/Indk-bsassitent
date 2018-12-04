05b7889b3e160e7430f1e00dba0e4c16a5f66174
MikkelFilip <mikkelfilip1@gmail.com>
2018-12-04 10:27:10 +0100

    recipes added to program

--- src/Services/Scripts/hest.java
+++ src/Services/Scripts/hest.java
@@ -3,6 +3,7 @@ package Services.Scripts;
 import GUI.*;
 import Indkøbsliste.Food.Product;
 import Searching.Searching;
+import recipes.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,6 +16,12 @@ public class hest {
         var sorted = new Searching();
 
         List <Product> sort = sorted.FuzzySearchByName("æble", test, 20);
+
+        List <recipes> AllRecipes = getRecipes.getList();
+
+        for (recipes recipe: AllRecipes){
+            System.out.println(recipe.ingredient.get(1));
+        }
 
         Login loginScreen = new Login();
         loginScreen.setVisible(true);
